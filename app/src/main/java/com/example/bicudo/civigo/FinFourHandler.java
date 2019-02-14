1c3509b5282311855dbd8996212f23a231faba90
liolau <xxliolauxx@gmail.com>
2019-02-14 15:35:50 +0100

    Added cookie Tracking Added skeleton functions to Handler

--- app/src/main/java/com/example/bicudo/civigo/FinFourHandler.java
+++ app/src/main/java/com/example/bicudo/civigo/FinFourHandler.java
@@ -2,6 +2,7 @@ package com.example.bicudo.civigo;
 
 import android.app.Application;
 import android.content.Context;
+import android.media.Image;
 
 //import com.android.volley.Request;
 import com.android.volley.Request;
@@ -12,15 +13,28 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.apache.http.client.CookieStore;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import org.json.JSONException;
 import org.json.JSONObject;
-import java.util.*;
+
+import com.android.volley.toolbox.*;
 
 public class FinFourHandler {
     private String baseUrl = "http://www.finfour.net";
     RequestQueue queue;
     public FinFourHandler(){
         this.queue = Volley.newRequestQueue(GlobalApplication.getAppContext());
+
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+
+        CookieStore cookieStore = new BasicCookieStore();
+        httpclient.setCookieStore( cookieStore );
+
+        HttpStack httpStack = new HttpClientStack( httpclient );
+        this.queue = Volley.newRequestQueue( GlobalApplication.getAppContext(), httpStack  );
     }
 
     public void Login() throws JSONException {
@@ -43,5 +57,20 @@ public class FinFourHandler {
                 }
         );
         this.queue.add(request);
+    }
+
+    //new post
+    public enum EntryType {Proposal, Mending};
+    public void newEntry(EntryType e, String description, Image image){
+    }
+
+    //timelines
+    public JSONObject getEntries(EntryType e){
+        return null;
+    }
+
+    //current users tokens
+    public JSONObject getTokens(){
+        return null;
     }
 }
