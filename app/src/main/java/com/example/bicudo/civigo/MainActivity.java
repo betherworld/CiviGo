fc46820074e2a259f36a1397490194e14d1b8d25
Davide Bernardi <berndavi@student.ethz.ch>
2019-02-14 10:05:30 +0100

    Navigation Bar changes

--- app/src/main/java/com/example/bicudo/civigo/MainActivity.java
+++ app/src/main/java/com/example/bicudo/civigo/MainActivity.java
@@ -17,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_proposals:
+                    mTextMessage.setText("Porposals");
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_fixes:
+                    mTextMessage.setText("Fixes");
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_map:
+                    mTextMessage.setText("Map");
                     return true;
             }
             return false;
