d125de0b67de7f99d08746ac11c9a88f5a559d5f
Davide Bernardi <berndavi@student.ethz.ch>
2019-02-14 14:47:04 +0100

    Login working without Cookies

--- app/src/main/java/com/example/bicudo/civigo/FinFourHandler.java
+++ app/src/main/java/com/example/bicudo/civigo/FinFourHandler.java
@@ -4,9 +4,11 @@ import android.app.Application;
 import android.content.Context;
 
 //import com.android.volley.Request;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -15,7 +17,7 @@ import org.json.JSONObject;
 import java.util.*;
 
 public class FinFourHandler {
-    private String baseUrl = "https://www.finfour.net";
+    private String baseUrl = "http://www.finfour.net";
     RequestQueue queue;
     public FinFourHandler(){
         this.queue = Volley.newRequestQueue(GlobalApplication.getAppContext());
@@ -23,10 +25,13 @@ public class FinFourHandler {
 
     public void Login() throws JSONException {
 
-        StringRequest request = new StringRequest(StringRequest.Method.POST, baseUrl + "/wapi/login",
-                new Response.Listener<String>() {
+        JSONObject params = new JSONObject();
+        params.put("email", "proposer@gmail.com");
+        params.put("password", "proposer");
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, baseUrl + "/wapi/login",params,
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(String response) {
+                    public void onResponse(JSONObject response) {
                         System.out.println(response.toString());
                     }
                 },
@@ -36,17 +41,7 @@ public class FinFourHandler {
                         System.err.println(error.toString());
                     }
                 }
-        ){
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("email", "proposer@gmail.com");
-                params.put("password", "proposer");
-                System.out.println(params.toString());
-
-                return params;
-            }
-        };
+        );
         this.queue.add(request);
     }
 }
--- app/src/main/java/com/example/bicudo/civigo/ProposalActivity.java
+++ app/src/main/java/com/example/bicudo/civigo/ProposalActivity.java
@@ -2,6 +2,7 @@ package com.example.bicudo.civigo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class ProposalActivity extends AppCompatActivity {
 
