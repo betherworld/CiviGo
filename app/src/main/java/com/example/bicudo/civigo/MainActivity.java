f30cbce84adb6bfbebc450372894c3237ca45c3c
davibicudo <davig@ethz.ch>
2019-02-14 11:37:28 +0100

    initial view change for map activity

--- app/src/main/java/com/example/bicudo/civigo/MainActivity.java
+++ app/src/main/java/com/example/bicudo/civigo/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.bicudo.civigo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,9 +27,8 @@ public class MainActivity extends AppCompatActivity {
                     mTextMessage.setText("Fixes");
                     return true;
                 case R.id.navigation_map:
-                    mTextMessage.setText("Map");
-                    return true;
-            }
+	    		    return true;
+            };
             return false;
         }
 
@@ -36,6 +38,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.navigation_map).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+		        Intent mapActivity = new Intent(MainActivity.this, MapActivity.class);
+	    	    startActivity(mapActivity);
+                    
+        	}
+        });
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
